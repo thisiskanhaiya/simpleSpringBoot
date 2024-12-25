1cc5fbde66e10f2faa9b5779192374718c36bd02
Kanhaiya <kgawade7878@gmail.com>
2024-12-25 18:01:14 +0530

    adding sql properties

--- src/main/java/com/project/simpleSpringboot/simpleSpringBoot/SimpleSpringBootApplication.java
+++ src/main/java/com/project/simpleSpringboot/simpleSpringBoot/SimpleSpringBootApplication.java
@@ -8,7 +8,7 @@ public class SimpleSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleSpringBootApplication.class, args);
-		System.out.println("Hello World");
+		
 	}
 
 }
--- src/main/java/com/project/simpleSpringboot/simpleSpringBoot/model/Product.java
+++ src/main/java/com/project/simpleSpringboot/simpleSpringBoot/model/Product.java
@@ -2,8 +2,10 @@ package com.project.simpleSpringboot.simpleSpringBoot.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "products")
 public class Product {
     @Id
     private int id;
